4ba6317f2e01871777edbee3dd068f23d9bce472
Srivatsan <vatsan1993@gmail.com>
2024-08-08 15:28:02 +0530

    finished phase 1

--- phase-1.js
+++ phase-1.js
@@ -1,7 +1,7 @@
 function stretch() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("done stretching");
+      console.log('done stretching');
       resolve();
     }, 1000);
   });
@@ -10,7 +10,7 @@ function stretch() {
 function runOnTreadmill() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("done running on treadmill");
+      console.log('done running on treadmill');
       resolve();
     }, 500);
   });
@@ -19,20 +19,28 @@ function runOnTreadmill() {
 function liftWeights() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("done lifting weights");
+      console.log('done lifting weights');
       resolve();
     }, 2000);
   });
 }
 
 // refactor this function to handle Promises using async/await instead of
-  // .then and .catch
-function workout() {
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
-    .catch((err) => console.log(err));
+// .then and .catch
+async function workout() {
+  // stretch()
+  //   .then(runOnTreadmill)
+  //   .then(liftWeights)
+  //   .then(() => console.log("done working out"))
+  //   .catch((err) => console.log(err));
+  try {
+    await stretch();
+    await runOnTreadmill();
+    await liftWeights();
+    console.log('done working out');
+  } catch (err) {
+    (err) => console.log(err);
+  }
 }
 
 /* ============================ TEST YOUR CODE ============================
@@ -41,10 +49,9 @@ Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
 workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
